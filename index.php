9d63140a114ea714df37c12cc2128c8758156087
Trapholov <trapholov@gmail.com>
2015-03-07 18:44:50 +0300

    Some fixes and styles were moved to css.

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ class madnews {
     array('message' => 'Омская полиция', 'sex' => 'f'),
     array('message' => 'Омич, стрелявший в прохожих из окна', 'sex' => 'm'),
     array('message' => 'Депутат Братска', 'sex' => 'm'),
-    array('message' => 'Группа томичей', 'sex' => 'f'),
+    array('message' => 'Группа томичей', 'sex' => 'plural'),
     array('message' => 'В Волгограде двое чеченцев', 'sex' => 'plural'),
   );
   private $action = array(
@@ -115,64 +115,20 @@ $mad = new madnews();
   <meta content="http://strangefoxes.ru/madnews/" property="og:url">
   <meta content="MAD News - Только драма" property="og:title">
   <meta content="Генератор самых драматических новостей в мире Breaking Mad. Сенсационные факты о Российской глубинке!" property="og:description">
+  <link href="style.css" rel="stylesheet" type="text/css">
 
 </head>
-<body style="margin:0;padding:0;">
-<div style="width: 1px; height: 314px;" class="flow-wrapper">
-  <div class="center-wrapper">
-    <div id="Stage">
+<body>
 
-      <div id="Stage_c0" style="background: none repeat scroll 0 0 #000;
-    bottom: auto;
-    color: rgb(255, 255, 255);
-    font-family: Arial Black,Gadget,sans-serif;
-    font-size: 24px;
-    font-weight: normal;
-    left: 679px;
-    margin: 0;
-    padding: 5px 15px;
-    position: absolute;
-    right: auto;
-    text-decoration: none;
-    top: 393px;
-    white-space: normal;" class="Stage_c0_id"><?php print $mad->getNew(3); ?></div>
-      <div id="Stage_b0" style="background: none repeat scroll 0 0 #000;
-    bottom: auto;
-    color: rgb(255, 255, 255);
-    font-family: Arial Black,Gadget,sans-serif;
-    font-size: 24px;
-    font-weight: normal;
-    left: 679px;
-    margin: 0;
-    padding: 5px 15px;
-    position: absolute;
-    right: auto;
-    text-decoration: none;
-    top: 340px;
-    white-space: normal;" class="Stage_b0_id"><?php print $mad->getNew(2); ?></div>
-      <div id="Stage_a0" style="background: none repeat scroll 0 0 #000;
-    bottom: auto;
-    color: rgb(255, 255, 255);
-    font-family: Arial Black,Gadget,sans-serif;
-    font-size: 24px;
-    font-weight: normal;
-    left: 679px;
-    margin: 0;
-    padding: 5px 15px;
-    position: absolute;
-    right: auto;
-    text-decoration: none;
-    top: 288px;
-    white-space: normal;" class="Stage_a0_id"><?php print $mad->getNew(1); ?></div>
+<div class="center-wrapper">
+  <div id="stage">
 
-      <div id="Stage_refresh4" style="position: absolute; margin: 0px; left: 679px; top: 442px; width: 42px; height: 42px; right: auto; bottom: auto; background-image: url('refresh.jpg'); background-color: transparent; background-repeat: no-repeat; background-size: 100% 100%; cursor: pointer; background-position: 0px 0px; display: block;" class="Stage_refresh4_id"></div>
-      <div id="Stage_refresh3" style="position: absolute; margin: 0px; width: 42px; height: 42px; left: 679px; top: 442px; right: auto; bottom: auto; z-index: 0; display: none;" class="Stage_refresh3_id">
-        <div id="Stage_refresh3_refresh2" style="position: absolute; margin: 0px; left: 0px; top: 0px; width: 42px; height: 42px; right: auto; bottom: auto; background-image: url('refresh2.jpg'); background-color: transparent; background-repeat: no-repeat; background-size: 100% 100%; cursor: pointer; background-position: 0px 0px; transform: translate(0px, 0px) rotate(0deg) scale(1, 1);" class="Stage_refresh3_refresh2_id"></div>
-      </div>
+    <div id="stage_c0" class="stage"><?php print $mad->getNew(3); ?></div>
+    <div id="stage_b0" class="stage"><?php print $mad->getNew(2); ?></div>
+    <div id="stage_a0" class="stage"><?php print $mad->getNew(1); ?></div>
 
-      </div>
-      </div>
-    </div>
+    <div id="stage_refresh"><a href="/madnews/"></a></div>
+
   </div>
 </div>
 
